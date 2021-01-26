5235ab7a39610a8bdb19ff480ece8a526b24b284
Marcelo Schneider <schneider.fei@gmail.com>
2021-01-26 15:02:51 -0300

    Adiciona horário ao eixo x da escala de pressão
    Apresenta horário e data no eixo x do gráfico de pressão.

--- js/src/Controllers/PressureSerial.class.js
+++ js/src/Controllers/PressureSerial.class.js
@@ -2,7 +2,6 @@ import { PressureSerialChart } from "../Charts/PressureSerialChart.class";
 import { BaseChart } from "./BaseChart.class";
 
 export class PressureSerial extends BaseChart {
-  
   static loadData(actions) {
     const dateFormat = "DD/MM/YYYY";
     let initialDate = moment().add(-1, "days").format(dateFormat);
@@ -10,18 +9,25 @@ export class PressureSerial extends BaseChart {
 
     let data = this.getDataHandler();
 
-    data.loadSerialPressureByDateInterval({ini: data.dateConvert(initialDate), end: data.dateConvert(endDate)}, pressure => {
+    data.loadSerialPressureByDateInterval(
+      { ini: data.dateConvert(initialDate), end: data.dateConvert(endDate) },
+      (pressure) => {
         if (actions) {
-            actions(pressure);
+          actions(pressure);
         }
       }
     );
   }
 
-
   static getChart(labels, dataLeft, dataRight, ctx, fontSize) {
-    let formattedDates = this.dateToBRFormat(labels, false);
-    let pressure = new PressureSerialChart(formattedDates, dataLeft, dataRight, ctx, fontSize);
+    let formattedDates = this.dateToBRFormat(labels, true);
+    let pressure = new PressureSerialChart(
+      formattedDates,
+      dataLeft,
+      dataRight,
+      ctx,
+      fontSize
+    );
 
     return pressure.getChart();
   }
